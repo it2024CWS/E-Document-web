e01e76fde3e1b230bc94006b912b9d3694ccd7bc
hackterIquri <hackter.ssv@gmail.com>
2026-02-12 23:10:35 +0700

    Update UploadButton.tsx

--- src/components/UploadButton/UploadButton.tsx
+++ src/components/UploadButton/UploadButton.tsx
@@ -149,18 +149,22 @@ const UploadButton = ({ parentFolderId, onUploadComplete }: UploadButtonProps) =
                     bgcolor: colors.dominant.white1,
                     color: colors.secondary.text,
                     textTransform: 'none',
+                    textAlign: 'center',
+                    fontSize: 25,
                     fontWeight: 500,
+                    height: 60,
+                    width: '80%',
                     boxShadow: '0 1px 2px 0 rgba(60,64,67,.3), 0 1px 3px 1px rgba(60,64,67,.15)',
                     '&:hover': {
                         bgcolor: colors.dominant.white3,
                         boxShadow: '0 1px 3px 0 rgba(60,64,67,.3), 0 4px 8px 3px rgba(60,64,67,.15)',
                     },
-                    borderRadius: '24px',
+                    borderRadius: '18px',
                     px: 3,
                     py: 1,
                 }}
             >
-                New
+                Add
             </Button>
 
             <Menu
